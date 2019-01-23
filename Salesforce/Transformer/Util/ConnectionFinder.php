2208e3a4e5e4817b45b1fa742ec87a5345f3887d
curiosity26 <curiosity26@gmail.com>
2019-01-23 12:12:51 -0500

    When compiling an Entity from an SObject, use the connection name when querying for existing entities

---
+++ Salesforce/Transformer/Util/ConnectionFinder.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex.boyce
+ * Date: 1/23/19
+ * Time: 10:18 AM
+ */
+
+namespace AE\ConnectBundle\Salesforce\Transformer\Util;
+
+use AE\ConnectBundle\Annotations\Connection;
+use AE\ConnectBundle\Connection\Dbal\ConnectionEntityInterface;
+use AE\ConnectBundle\Metadata\Metadata;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\Reader;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\ORMException;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class ConnectionFinder implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    /**
+     * @var RegistryInterface
+     */
+    private $registry;
+
+    /**
+     * @var Reader
+     */
+    private $reader;
+
+    public function __construct(RegistryInterface $registry, Reader $reader, ?LoggerInterface $logger = null)
+    {
+        $this->registry = $registry;
+        $this->reader   = $reader;
+
+        $this->setLogger($logger ?: new NullLogger());
+        AnnotationRegistry::loadAnnotationClass(Connection::class);
+    }
+
+    public function find(string $connectionName, Metadata $metadata): ?ConnectionEntityInterface
+    {
+        try {
+            if (null === ($fieldMetadata = $metadata->getConnectionNameField())) {
+                return null;
+            }
+
+            $class   = $metadata->getClassName();
+            $prop    = $fieldMetadata->getProperty();
+            $manager = $this->registry->getManagerForClass($class);
+            /** @var ClassMetadata $classMetadata */
+            $classMetadata = $manager->getClassMetadata($class);
+            $assoc         = $classMetadata->getAssociationMapping($prop);
+            $connClass     = $assoc['targetEntity'];
+            $connManager   = $this->registry->getManagerForClass($connClass);
+            $repo          = $connManager->getRepository($connClass);
+            /** @var ClassMetadata $connMetadata */
+            $connMetadata    = $connManager->getClassMetadata($connClass);
+            $connectionField = 'connection';
+            $connection      = null;
+
+            // Look for fields on the target entity that have the Connection annotation
+            /** @var \ReflectionProperty $field */
+            foreach ($connMetadata->getReflectionProperties() as $field) {
+                foreach ($this->reader->getPropertyAnnotations($field) as $annotation) {
+                    if ($annotation instanceof Connection) {
+                        $connectionField = $field->getName();
+                        break;
+                    }
+                }
+            }
+
+            if ($connMetadata->hasField($connectionField)) {
+                // If the entity has a field named 'connection' or uses the Connection annotation on the connection
+                // name field,then we can easily do a lookup
+                /** @var ConnectionEntityInterface $connection */
+                $connection = $repo->findOneBy([$connectionField => $connectionName]);
+            } else {
+                // If we can't easily determine which field uses the connection name, we have to look at all entities
+                $connections = $repo->findAll();
+                foreach ($connections as $conn) {
+                    if ($conn instanceof ConnectionEntityInterface && $conn->getName() === $connectionName) {
+                        $connection = $conn;
+                        break;
+                    }
+                }
+            }
+
+            return $connection;
+        } catch (ORMException $e) {
+            $this->logger->warning($e->getMessage());
+            $this->logger->debug($e->getTraceAsString());
+
+            return null;
+        }
+    }
+}
---
+++ Tests/Salesforce/Transformer/Util/ConnectionFinderTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex.boyce
+ * Date: 1/23/19
+ * Time: 10:34 AM
+ */
+
+namespace AE\ConnectBundle\Tests\Salesforce\Transformer\Util;
+
+use AE\ConnectBundle\Connection\Dbal\ConnectionEntityInterface;
+use AE\ConnectBundle\Manager\ConnectionManagerInterface;
+use AE\ConnectBundle\Salesforce\Transformer\Util\ConnectionFinder;
+use AE\ConnectBundle\Tests\DatabaseTestCase;
+use AE\ConnectBundle\Tests\Entity\Account;
+use AE\ConnectBundle\Tests\Entity\Contact;
+use AE\ConnectBundle\Tests\Entity\Order;
+
+class ConnectionFinderTest extends DatabaseTestCase
+{
+    /**
+     * @var ConnectionFinder
+     */
+    private $connectionFinder;
+
+    /**
+     * @var ConnectionManagerInterface
+     */
+    private $connectionManager;
+
+    protected function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        parent::setUp();
+        $this->connectionFinder = $this->get(ConnectionFinder::class);
+        $this->connectionManager = $this->get(ConnectionManagerInterface::class);
+    }
+
+    public function testFindForAccount()
+    {
+        $this->loadOrgConnections();
+        $connection = $this->connectionManager->getConnection('db_test_org1');
+
+        $this->assertNotNull($connection);
+
+        $metadata = $connection->getMetadataRegistry()->findMetadataByClass(Account::class);
+
+        $this->assertNotNull($metadata);
+
+        $entity = $this->connectionFinder->find('db_test_org1', $metadata);
+
+        $this->assertNotNull($entity);
+        $this->assertInstanceOf(ConnectionEntityInterface::class, $entity);
+        $this->assertEquals('db_test_org1', $entity->getName());
+    }
+
+    public function testFindForContact()
+    {
+        $this->loadOrgConnections();
+        $connection = $this->connectionManager->getConnection('db_test_org1');
+
+        $this->assertNotNull($connection);
+
+        $metadata = $connection->getMetadataRegistry()->findMetadataByClass(Contact::class);
+
+        $this->assertNotNull($metadata);
+
+        $entity = $this->connectionFinder->find('db_test_org1', $metadata);
+
+        $this->assertNotNull($entity);
+        $this->assertInstanceOf(ConnectionEntityInterface::class, $entity);
+        $this->assertEquals('db_test_org1', $entity->getName());
+    }
+
+    public function testFindOnClassWithNoConnection()
+    {
+        $this->loadOrgConnections();
+        $connection = $this->connectionManager->getConnection('default');
+
+        $this->assertNotNull($connection);
+
+        $metadata = $connection->getMetadataRegistry()->findMetadataByClass(Order::class);
+
+        $this->assertNotNull($metadata);
+
+        $entity = $this->connectionFinder->find('default', $metadata);
+
+        $this->assertNull($entity);
+    }
+}
