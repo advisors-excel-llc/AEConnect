aa4faff743ee2651ef36749b59e826e55daeb29a
curiosity26 <curiosity26@gmail.com>
2019-02-05 15:14:01 -0500

    Prevent max depth error if the listener receives 256+ exceptions

--- Command/ListenCommand.php
+++ Command/ListenCommand.php
@@ -56,19 +56,22 @@ class ListenCommand extends Command implements LoggerAwareInterface
         if (null === $connection) {
             throw new \InvalidArgumentException("Could not find any connection named '$connectionName'.");
         }
+
         $output->writeln('<info>Listening to connection: '.$connectionName.'</info>');
 
-        $this->start($connection, $output);
+        while ($this->start($connection)) {
+            $output->writeln('<info>Restarting connection: '.$connectionName.'</info>');
+        }
     }
 
-    private function start(ConnectionInterface $connection, OutputInterface $output)
+    private function start(ConnectionInterface $connection)
     {
         try {
             $connection->getStreamingClient()->start();
         } catch (\Exception $e) {
             $this->logger->critical($e->getMessage());
-            $output->writeln('<info>Restarting connection: '.$connection->getName().'</info>');
-            $this->start($connection, $output);
         }
+
+        return true;
     }
 }
