92aa5076f5bb70b80824c114032016c9dce6db82
Alex Boyce <curiosity26@gmail.com>
2019-03-12 12:39:58 -0400

    Update rest sdk to version 1.3. Update the Auth Providers and ReplayExtension configuration (#159)

--- AuthProvider/OAuthProvider.php
+++ AuthProvider/OAuthProvider.php
@@ -9,20 +9,12 @@ namespace AE\ConnectBundle\AuthProvider;
  * Time: 2:59 PM
  */
 
-use AE\SalesforceRestSdk\AuthProvider\OAuthProvider as BaseAuthProvider;
+use AE\SalesforceRestSdk\AuthProvider\CachedOAuthProvider;
 use Doctrine\Common\Cache\CacheProvider;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
-use Psr\Log\NullLogger;
+use Symfony\Component\Cache\Adapter\DoctrineAdapter;
 
-class OAuthProvider extends BaseAuthProvider implements LoggerAwareInterface
+class OAuthProvider extends CachedOAuthProvider
 {
-    use LoggerAwareTrait;
-    /**
-     * @var CacheProvider
-     */
-    private $cache;
-
     public function __construct(
         CacheProvider $cache,
         string $clientId,
@@ -30,89 +22,20 @@ class OAuthProvider extends BaseAuthProvider implements LoggerAwareInterface
         string $url,
         ?string $username,
         ?string $password,
-        string $grantType = BaseAuthProvider::GRANT_PASSWORD,
+        string $grantType = self::GRANT_PASSWORD,
         ?string $redirectUri = null,
         ?string $code = null
     ) {
-        parent::__construct($clientId, $clientSecret, $url, $username, $password, $grantType, $redirectUri, $code);
-        $this->cache  = $cache;
-        $this->logger = new NullLogger();
-    }
-
-    public function authorize($reauth = false): string
-    {
-        $oldToken = null;
-        $ref      = new \ReflectionClass(BaseAuthProvider::class);
-
-        if (!$reauth && null === $this->token && $this->cache->contains($this->clientId)) {
-            $values             = $this->cache->fetch($this->clientId);
-            $this->token        = $oldToken = $values['token'];
-            $this->tokenType    = $values['tokenType'];
-            $this->refreshToken = $values['refreshToken'];
-
-            $instUrl = $ref->getProperty('instanceUrl');
-            $instUrl->setAccessible(true);
-            $instUrl->setValue($this, $values['instanceUrl']);
-
-            $idUrl = $ref->getProperty('identityUrl');
-            $idUrl->setAccessible(true);
-            $idUrl->setValue($this, $values['identityUrl']);
-
-            $prop = $ref->getProperty('isAuthorized');
-            $prop->setAccessible(true);
-            $prop->setValue($this, true);
-        }
-
-        try {
-            $header = parent::authorize($reauth);
-
-            if ($this->token !== $oldToken) {
-                $prop = $ref->getProperty('identityUrl');
-                $prop->setAccessible(true);
-
-                $this->cache->save(
-                    $this->clientId,
-                    [
-                        'tokenType'    => $this->tokenType,
-                        'token'        => $this->token,
-                        'instanceUrl'  => $this->getInstanceUrl(),
-                        'refreshToken' => $this->getRefreshToken(),
-                        'identityUrl'  => $prop->getValue($this),
-                    ]
-                );
-            }
-
-            return $header;
-        } catch (\Exception $e) {
-            $this->logger->critical($e->getMessage());
-            $this->revoke();
-        }
-
-        return '';
-    }
-
-    public function revoke(): void
-    {
-        if ($this->cache->contains($this->clientId)) {
-            $this->cache->delete($this->clientId);
-        }
-
-        parent::revoke();
-    }
-
-    /**
-     * @return string
-     */
-    public function getClientId(): string
-    {
-        return $this->clientId;
-    }
-
-    /**
-     * @return string
-     */
-    public function getClientSecret(): string
-    {
-        return $this->clientSecret;
+        parent::__construct(
+            new DoctrineAdapter($cache),
+            $clientId,
+            $clientSecret,
+            $url,
+            $username,
+            $password,
+            $grantType,
+            $redirectUri,
+            $code
+        );
     }
 }
--- AuthProvider/SoapProvider.php
+++ AuthProvider/SoapProvider.php
@@ -8,92 +8,18 @@
 
 namespace AE\ConnectBundle\AuthProvider;
 
-use AE\SalesforceRestSdk\AuthProvider\SoapProvider as BaseAuthProvider;
+use AE\SalesforceRestSdk\AuthProvider\CachedSoapProvider;
 use Doctrine\Common\Cache\CacheProvider;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
-use Psr\Log\NullLogger;
+use Symfony\Component\Cache\Adapter\DoctrineAdapter;
 
-class SoapProvider extends BaseAuthProvider implements LoggerAwareInterface
+class SoapProvider extends CachedSoapProvider
 {
-    use LoggerAwareTrait;
-
-    /**
-     * @var CacheProvider
-     */
-    private $cache;
-
     public function __construct(
         CacheProvider $cache,
         string $username,
         string $password,
         string $url = 'https://login.salesforce.com/'
     ) {
-        parent::__construct($username, $password, $url);
-        $this->cache  = $cache;
-        $this->logger = new NullLogger();
-    }
-
-    public function authorize($reauth = false)
-    {
-        $key      = "SOAP_{$this->username}";
-        $oldToken = null;
-        $ref      = new \ReflectionClass(BaseAuthProvider::class);
-
-        if (!$reauth && null === $this->token && $this->cache->contains($key)) {
-            $values = $this->cache->fetch($key);
-
-            $this->token     = $oldToken = $values['token'];
-            $this->tokenType = $values['tokenType'];
-
-            $instUrl = $ref->getProperty('instanceUrl');
-            $instUrl->setAccessible(true);
-            $instUrl->setValue($this, $values['instanceUrl']);
-
-            $idUrl = $ref->getProperty('identityUrl');
-            $idUrl->setAccessible(true);
-            $idUrl->setValue($this, $values['identityUrl']);
-
-            $prop = $ref->getProperty('isAuthorized');
-            $prop->setAccessible(true);
-            $prop->setValue($this, true);
-        }
-
-        try {
-            $header = parent::authorize($reauth);
-
-            if ($this->token !== $oldToken) {
-                $prop = $ref->getProperty('identityUrl');
-                $prop->setAccessible(true);
-
-                $this->cache->save(
-                    $key,
-                    [
-                        'tokenType'   => $this->tokenType,
-                        'token'       => $this->token,
-                        'instanceUrl' => $this->getInstanceUrl(),
-                        'identityUrl' => $prop->getValue($this),
-                    ]
-                );
-            }
-
-            return $header;
-        } catch (\Exception $e) {
-            $this->logger->critical($e->getMessage());
-            $this->revoke();
-
-            return '';
-        }
-    }
-
-    public function revoke(): void
-    {
-        $key = "SOAP_{$this->username}";
-
-        if ($this->cache->contains($key)) {
-            $this->cache->delete($key);
-        }
-
-        parent::revoke();
+        parent::__construct(new DoctrineAdapter($cache), $username, $password, $url);
     }
 }
