2f990f3b602ac79a562a2752ef199864e918648a
curiosity26 <curiosity26@gmail.com>
2019-06-07 11:12:40 -0400

    DebugStreamingCommand shouldn't use the same extensions as the app's client. Rather, it should use the configuration from the app's client to build its own client and extensions. Also, allow for the command to take a replay Id as an option

--- Command/DebugStreamingCommand.php
+++ Command/DebugStreamingCommand.php
@@ -16,22 +16,47 @@ use AE\ConnectBundle\Streaming\GenericEvent;
 use AE\ConnectBundle\Streaming\PlatformEvent;
 use AE\ConnectBundle\Streaming\Topic;
 use AE\SalesforceRestSdk\Bayeux\BayeuxClient;
+use AE\SalesforceRestSdk\Bayeux\Extension\ReplayExtension;
+use AE\SalesforceRestSdk\Bayeux\Extension\SfdcExtension;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class DebugStreamingCommand extends Command
+class DebugStreamingCommand extends Command implements LoggerAwareInterface, ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+    use LoggerAwareTrait;
+
     /**
      * @var ConnectionManagerInterface
      */
     private $connectionManager;
 
-    public function __construct(ConnectionManagerInterface $connectionManager)
-    {
-        parent::__construct('debug:ae_connect:stream');
+    /**
+     * @var SfdcExtension
+     */
+    private $sfdcExtension;
+
+    public function __construct(
+        ConnectionManagerInterface $connectionManager,
+        SfdcExtension $sfdcExtension,
+        ?LoggerInterface $logger = null
+    ) {
+        parent::__construct('debug:ae_connect:stream')
+              ->addOption('replay-id', 'r', InputOption::VALUE_OPTIONAL)
+        ;
+
         $this->connectionManager = $connectionManager;
+        $this->sfdcExtension     = $sfdcExtension;
+        $this->logger            = $logger ?: new NullLogger();
     }
 
     /**
@@ -56,10 +81,21 @@ class DebugStreamingCommand extends Command
             return;
         }
 
-        $client = $connection->getStreamingClient();
+        $replayExtension = $this->container->get("ae_connect.connection.$connectionName.replay_extension");
+        $replayId        = $input->getOption('replay-id');
+        $replayExt       = new ReplayExtension($replayId ?: $replayExtension->getReplayId());
+        $client          = $connection->getStreamingClient();
         /** @var BayeuxClient $bayeuxClient */
-        $bayeuxClient = $client->getClient();
-        $debugClient  = new Client($bayeuxClient);
+        $bayeuxClient = new BayeuxClient(
+            $client->getClient()->getTransport(),
+            $client->getClient()->getAuthProvider(),
+            $this->logger
+        );
+
+        $bayeuxClient->addExtension($this->sfdcExtension);
+        $bayeuxClient->addExtension($replayExt);
+
+        $debugClient = new Client($bayeuxClient);
 
         foreach ($client->getChannelSubscribers() as $subscriber) {
             switch (get_class($subscriber)) {
