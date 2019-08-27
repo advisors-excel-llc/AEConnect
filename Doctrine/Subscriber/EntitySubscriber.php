345a5311681618faba1c233e2b0a4868ee4519a3
Alex Boyce <alex.boyce@advisorsexcel.com>
2019-08-27 14:07:03 -0400

    Remove cache provider from within the EntitySubscriber. Abstract the entity subscriber for easier extension. Test the entity subscriber. (#205)

--- Doctrine/Subscriber/EntitySubscriber.php
+++ Doctrine/Subscriber/EntitySubscriber.php
@@ -2,262 +2,84 @@
 /**
  * Created by PhpStorm.
  * User: alex.boyce
- * Date: 10/18/18
- * Time: 4:44 PM
+ * Date: 8/27/19
+ * Time: 12:14 PM
  */
 
 namespace AE\ConnectBundle\Doctrine\Subscriber;
 
-use AE\ConnectBundle\Connection\Dbal\SalesforceIdEntityInterface;
-use AE\ConnectBundle\Manager\ConnectionManagerInterface;
-use AE\ConnectBundle\Salesforce\Outbound\Compiler\CompilerResult;
-use AE\ConnectBundle\Salesforce\Outbound\Compiler\SObjectCompiler;
-use AE\ConnectBundle\Salesforce\SalesforceConnector;
-use Doctrine\Common\Cache\CacheProvider;
-use Doctrine\Common\EventSubscriber;
-use Doctrine\ORM\Event\OnClearEventArgs;
-use Doctrine\ORM\Event\LifecycleEventArgs;
-use Doctrine\ORM\Event\PostFlushEventArgs;
-use Doctrine\ORM\Event\PreFlushEventArgs;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
-
-class EntitySubscriber implements EventSubscriber, LoggerAwareInterface
+class EntitySubscriber extends AbstractEntitySubscriber
 {
-    use LoggerAwareTrait;
+    /**
+     * @var array
+     */
+    protected $upserts    = [];
 
     /**
-     * @var SalesforceConnector
+     * @var array
      */
-    private $connector;
+    protected $removals   = [];
 
     /**
-     * @var ConnectionManagerInterface
+     * @var array
      */
-    private $connectionManager;
+    protected $processing = [];
 
-    /**
-     * @var SObjectCompiler
-     */
-    private $compiler;
-
-    /**
-     * @var CacheProvider
-     */
-    private $cache;
-
-    public function __construct(
-        SalesforceConnector $connector,
-        ConnectionManagerInterface $connectionManager,
-        SObjectCompiler $compiler,
-        CacheProvider $cache
-    ) {
-        $this->connector         = $connector;
-        $this->connectionManager = $connectionManager;
-        $this->compiler          = $compiler;
-        $this->cache             = $cache;
-        $this->logger            = new NullLogger();
-    }
-
-    public function getSubscribedEvents()
+    protected function getUpserts(): array
     {
-        return [
-            'postPersist',
-            'postUpdate',
-            'postRemove',
-            'preFlush',
-            'postFlush',
-            'onClear',
-        ];
+        return $this->upserts;
     }
 
-    public function postPersist(LifecycleEventArgs $event)
+    protected function getRemovals(): array
     {
-        $entity = $event->getEntity();
-        $this->upsertEntity($entity);
+        return $this->removals;
     }
 
-    public function postUpdate(LifecycleEventArgs $event)
+    protected function getProcessing(): array
     {
-        $entity = $event->getEntity();
-        $this->upsertEntity($entity);
+        return $this->processing;
     }
 
-    public function postRemove(LifecycleEventArgs $event)
+    protected function saveUpserts(array $upserts)
     {
-        $entity = $event->getEntity();
-        $this->removeEntity($entity);
+        $this->upserts = $upserts;
+
+        return $this;
     }
 
-    public function preFlush(PreFlushEventArgs $event)
+    protected function saveRemovals(array $removals)
     {
-        $this->flushUpserts();
-        $this->flushRemovals();
+        $this->removals = $removals;
+
+        return $this;
     }
 
-    public function postFlush(PostFlushEventArgs $event)
+    protected function saveProcessing(array $processing)
     {
-        $this->cache->save('processing', []);
+        $this->processing = $processing;
+
+        return $this;
     }
 
-    public function onClear(OnClearEventArgs $event)
+    protected function clearUpserts()
     {
-        $this->cache->deleteAll();
+        $this->upserts = [];
+
+        return $this;
     }
 
-    /**
-     * @param LoggerInterface $logger
-     */
-    public function setLogger(?LoggerInterface $logger): void
+    protected function clearRemovals()
     {
-        $this->logger = $logger ?: new NullLogger();
+        $this->removals = [];
+
+        return $this;
     }
 
-    /**
-     * @param $entity
-     */
-    protected function upsertEntity($entity): void
+    protected function clearProcessing()
     {
-        if ($entity instanceof SalesforceIdEntityInterface) {
-            return;
-        }
+        $this->processing = [];
 
-        $entities = [];
-        if ($this->cache->contains('upserts')) {
-            $entities = $this->cache->fetch('upserts');
-        }
-
-        $oid = \spl_object_hash($entity);
-
-        if (!array_key_exists($oid, $entities)) {
-            $entities[$oid] = $entity;
-        }
-
-        $this->cache->save('upserts', $entities);
+        return $this;
     }
 
-    /**
-     * @param $entity
-     */
-    protected function removeEntity($entity): void
-    {
-        if ($entity instanceof SalesforceIdEntityInterface) {
-            return;
-        }
-
-        $entities = [];
-        if ($this->cache->contains('removals')) {
-            $entities = $this->cache->fetch('removals');
-        }
-
-        $oid = \spl_object_hash($entity);
-
-        if (!array_key_exists($oid, $entities)) {
-            $entities[$oid] = $entity;
-        }
-
-        $this->cache->save('removals', $entities);
-    }
-
-    protected function flushUpserts(): void
-    {
-        if (!$this->cache->contains('upserts')) {
-            return;
-        }
-
-        $entities = $this->cache->fetch('upserts');
-
-        if (empty($entities)) {
-            return;
-        }
-
-        $processing = [];
-
-        if ($this->cache->contains('processing')) {
-            $processing = $this->cache->fetch('processing');
-        }
-
-        $connections = $this->connectionManager->getConnections();
-        foreach ($connections as $connection) {
-            foreach ($entities as $entity) {
-                $oid = \spl_object_id($entity);
-                try {
-                    if (null === $connection->getMetadataRegistry()->findMetadataForEntity($entity)) {
-                        continue;
-                    }
-
-                    if (array_key_exists($oid, $processing)) {
-                        continue;
-                    }
-
-                    $processing[$oid] = $oid;
-                    $this->connector->send($entity, $connection->getName());
-                } catch (\RuntimeException $e) {
-                    // If the entity isn't able to be sent to Salesforce for any reason,
-                    // a RuntimeException is thrown. We don't want that stopping our fun.
-                    $this->logger->error($e->getMessage());
-                    $this->logger->debug($e->getTraceAsString());
-                    unset($processing[$oid]);
-                }
-                unset($entities[$oid]);
-            }
-        }
-
-        $this->cache->save('upserts', $entities);
-        $this->cache->save('processing', $processing);
-    }
-
-    protected function flushRemovals(): void
-    {
-        if (!$this->cache->contains('removals')) {
-            return;
-        }
-
-        $entities = $this->cache->fetch('removals');
-
-        if (empty($entities)) {
-            return;
-        }
-        $processing = [];
-
-        if ($this->cache->contains('processing')) {
-            $processing = $this->cache->fetch('processing');
-        }
-
-        $connections = $this->connectionManager->getConnections();
-        foreach ($connections as $connection) {
-            foreach ($entities as $entity) {
-                $oid = \spl_object_id($entity);
-                try {
-                    if (null === $connection->getMetadataRegistry()->findMetadataForEntity($entity)) {
-                        continue;
-                    }
-
-                    if (array_key_exists($oid, $processing)) {
-                        continue;
-                    }
-
-                    $processing[$oid] = $oid;
-                    $result = $this->compiler->compile($entity, $connection->getName());
-                    $result->setIntent(CompilerResult::DELETE);
-                    if (null !== $result->getSObject()->Id) {
-                        $this->connector->sendCompilerResult($result);
-                    }
-                } catch (\RuntimeException $e) {
-                    // If the entity isn't able to be sent to Salesforce for any reason,
-                    // a RuntimeException is thrown. We don't want that stopping our fun.
-                    $this->logger->error($e->getMessage());
-                    $this->logger->debug($e->getTraceAsString());
-                    unset($processing[$oid]);
-                } finally {
-                    unset($entities[$oid]);
-                }
-            }
-        }
-
-        $this->cache->save('removals', $entities);
-        $this->cache->save('processing', $processing);
-    }
 }
