8095fe089a5ec31b0e988209d39bfbc50fafedde
Nicholas Lafrance <nickalafrance@gmail.com>
2020-04-13 17:15:29 -0500

    Adds support needed for limit / offset

--- Salesforce/Synchronize/Handlers/GetQueryCounts.php
+++ Salesforce/Synchronize/Handlers/GetQueryCounts.php
@@ -9,29 +9,34 @@ class GetQueryCounts implements SyncHandler
 {
     public function process(SyncEvent $event): void
     {
-        $unwantedQueryParts = [' ORDER BY '];
+        $unwantedQueryParts = ['ORDER BY', 'LIMIT', 'OFFSET'];
         $connection = $event->getConnection();
         $client = $connection->getRestClient()->getSObjectClient();
         foreach ($event->getConfig()->getQueries() as $target => $query) {
             $targetObj = new Target();
-            $countQuery = 'SELECT COUNT(Id) ' . substr($query, strpos($query, 'FROM'));
+            $countQuery = 'SELECT COUNT(Id) '.substr($query, strpos($query, 'FROM'));
+            //First of all lets check if we have LIMIT / OFFSET stated.
+            $limit = PHP_INT_MAX;
+            $offset = 0;
+            //We'll record the limit and offset numbers here
+            if (preg_match('/(?<=LIMIT )[\d]+/', $query, $matches)) {
+                $limit = (int) $matches[0];
+            }
+            if (preg_match('/(?<=OFFSET )[\d]+/', $query, $matches)) {
+                $offset = (int) $matches[0];
+            }
 
             // We need to trim off any unwanted parts of the query, although we will know how many results we are going to
             // get if we already have limit + offset.
             foreach ($unwantedQueryParts as $part) {
-                $index = strpos($countQuery, $part);
-                if ($index) {
-                    $countQuery = substr($countQuery, 0, $index);
-                }
+                $countQuery = preg_replace('/'.$part.'[\s]+[^\s,]+(,[\s]*[\S]+)*/', '', $countQuery);
             }
 
             $response = $client->query($countQuery);
             $targetObj->name = $target;
             $targetObj->query = $query;
-            $targetObj->count = $response->getRecords()[0]->getExpr0();
+            $targetObj->count = min($response->getRecords()[0]->getExpr0() - $offset, $limit);
             $event->addTarget($targetObj);
         }
     }
-
-
 }
